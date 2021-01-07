1d19465f053c748ebf8691a9774285830b8287da
AshakaE <ashakaegerega@gmail.com>
2021-01-07 14:58:28 +0100

    corrected rubocop error

--- script.rb
+++ script.rb
@@ -1,9 +1,11 @@
 module Enumerable
-  def my_each(&block)
-    each(&block)
+  def my_each
+    for i in self
+      yield(i)
+    end
     self
   end
 end
 
 p(1..10).my_each { |x| p x }
-p([3, 4, 6, 9].my_each) { |x| p "we have #{x}" }
+p([3, 4, 6, 9]).my_each { |x| p "we have #{x}" }
