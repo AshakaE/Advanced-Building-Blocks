dbb94273ced51d7cf07046e8bc67bee0758c499d
AshakaE <ashakaegerega@gmail.com>
2021-01-08 10:07:06 +0100

    corrected linter error for my_each

--- script.rb
+++ script.rb
@@ -4,10 +4,8 @@ module Enumerable
 
     arr = self if self.class == Array
     arr.length.times { |item| yield(arr[item]) }
-
   end
 end
 
 p([1, 2, 3, 4, 5]).my_each { |x| p x }
 # p([3, 4, 6, 9]).my_each { |x| p "we have #{x}" }
-
