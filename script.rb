8db4a63d089dd75dce91adb79289f3e0215a4459
AshakaE <ashakaegerega@gmail.com>
2021-01-08 13:57:17 +0100

    built my_any? method

--- script.rb
+++ script.rb
@@ -32,11 +32,20 @@ module Enumerable
     end
     condition
   end
+
+  def my_any?
+    condition = false
+    my_each do |item|
+      condition = true if (block_given? && yield(item)) || (!block_given? && item)
+    end
+    condition
+  end
 end
 
 # p([1, 2, 3, 4, 5]).my_each { |x| p x }
 # p([3, 4, 6, 9]).my_each_with_index { |item, index| p "we have index #{index} for array #{item}" }
-# state = %w[ant bear cat]
-# p state.my_all? { |word| word.length < 3 }
+# state = %w[ant bear cat ja]
+# p state.my_any? { |word| word.length < 3 }
+# p state.my_any?
 # friends = %w[Sharon Leo Leila Brian Arun]
 # p friends.my_select { |friend| friend != 'Brian' }
