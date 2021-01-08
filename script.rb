d718cc51839b0b361d68dd3ab0f4603a131c33b8
AshakaE <ashakaegerega@gmail.com>
2021-01-08 10:31:17 +0100

    built each_with_index method

--- script.rb
+++ script.rb
@@ -2,10 +2,17 @@ module Enumerable
   def my_each
     return to_enum unless block_given?
 
-    arr = self if self.class == Array
+    arr = self if instance_of?(Array)
     arr.length.times { |item| yield(arr[item]) }
+  end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    arr = self if instance_of?(Array)
+    arr.length.times { |item| yield(arr[item], item) }
   end
 end
 
-p([1, 2, 3, 4, 5]).my_each { |x| p x }
-# p([3, 4, 6, 9]).my_each { |x| p "we have #{x}" }
+# p([1, 2, 3, 4, 5]).my_each { |x| p x }
+# p([3, 4, 6, 9]).my_each_with_index { |item, index| p "we have index #{index} for array #{item}" }
