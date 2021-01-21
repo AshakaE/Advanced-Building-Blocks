7cf4c02e440df014cd770abdfda63ddd04c298b6
AshakaE <ashakaegerega@gmail.com>
2021-01-21 19:49:51 +0100

    created tests for my_each

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,27 @@
+require './script'
+require 'rspec'
+
+RSpec.describe Enumerable do
+  let(:arr) { [6, 4, 3, 5] }
+  let(:range) { (1..8) }
+  let(:string) { %w[red green factor] }
+  let(:block) { proc { |num| num < 4 } }
+
+  describe "#my_each" do
+    context 'when no block is given' do
+      it { is_expected.to be_truthy }
+    end
+
+    it 'returns the original array' do
+      expect(string.my_each { |x| x }).to eq(string)
+    end
+
+    it 'prints result to stdout from block calculation' do
+      expect { arr.my_each { |i| print i + 4 } }.to output(/10879/).to_stdout
+    end
+
+    it 'prints result to stdout with block given' do
+      expect { string.my_each { |i| print i.length } }.to output(/356/).to_stdout
+    end
+  end 
+end
